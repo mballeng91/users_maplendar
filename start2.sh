2adc7064e5546dffe5d87d19daef8b292c47f95e
Miguel Ballén <migaballengal@unal.edu.co>
2017-11-16 01:15:06 -0500

    just testing

--- start2.sh
+++ start2.sh
@@ -1,8 +1,1 @@
-./rancher-compose --project-name ms-autenticateuser \
---url http://192.168.99.100:8080/v1/projects/1a5 \
---access-key 12DD53C770249D7BDB7B \
---secret-key mVqhDv7XRvFKSwCPyrmf3qLaC4WLr59adJj3C87g \
--f docker-compose.yml \
---verbose up \
--d --force-upgrade \
---confirm-upgrade
+./rancher-compose --project-name sa-api-gateway --url http://192.168.99.100:8080/v1/projects/1a5 --access-key 12DD53C770249D7BDB7B --secret-key mVqhDv7XRvFKSwCPyrmf3qLaC4WLr59adJj3C87g -f docker-compose.yml --verbose up -d --force-upgrade --confirm-upgrade
